0f7d7d1b545c754851256d96cb208f519f703291
vino <vino0908@outlook.com>
2023-07-06 23:00:44 +0800

    添加注释

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -13,6 +13,12 @@ const ReactElement = function (type: Type, key: Key, ref: Ref, props: Props): Re
   return element
 }
 
+/*
+* babel 会将 <App key="key" props={props} ref={ref}>1111</App> 转换为 jsx(App, {key: 'key', props: props, ref: ref}, '1111')
+*
+* 最新的会将key 作为第三个参数，见： https://babeljs.io/repl#?browsers=defaults&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=DwQQDmAEDWCmCeBeARHezIHsB2BjANgJa7SIDeAFgIbYAm-sAwkSQL6QBOsAZuV9-zIA6EWA6YwAZ3YAXeGFgoqy5AD4AUJEjBahAG6qAjMAD0ug5u3nVAJlPX1p8GFVA&debug=false&forceAllTransforms=false&modules=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=react%2Ctypescript&prettier=false&targets=&version=7.19.5&externalPlugins=&assumptions=%7B%7D
+*/
+
 export function jsx(type: ElementType, config: any, ...maybeChildren: any) {
   let key: Key = null
   const props: Props = {}
