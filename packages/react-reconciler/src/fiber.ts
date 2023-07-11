8feeef4dc8c84cb025e7a36be00ef282408719d2
vino <vino0908@outlook.com>
2023-07-11 23:23:10 +0800

    添加reconciler

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,8 @@
+import { Key, Props } from "shared/ReactTypes";
+import { WorkTag } from "./workTags";
+
+export class FiberNode {
+  constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+
+  }
+}
--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,7 +1,7 @@
 import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols'
 import type { ElementType, Key, Props, ReactElementType, Ref, Type } from 'shared/ReactTypes'
 
-const ReactElement = function (type: Type, key: Key, ref: Ref, props: Props): ReactElementType {
+const ReactElement = function(type: Type, key: Key, ref: Ref, props: Props): ReactElementType {
   const element = {
     $$typeof: REACT_ELEMENT_TYPE,
     type,
