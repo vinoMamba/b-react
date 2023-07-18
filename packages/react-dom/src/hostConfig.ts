710c7b205eb4e587c959ebeff6391b34037c74e7
vino <vino0908@outlook.com>
2023-07-19 00:07:06 +0800

    react-dom

---
+++ packages/react-dom/src/hostConfig.ts
@@ -0,0 +1,18 @@
+export type Container = Element
+export type Instance = Element
+
+export function createInstance(type: string, _props: any): Instance {
+  // TODO: props
+  const element = document.createElement(type)
+  return element
+}
+
+export function appendInitialChild(parent: Instance | Container, child: Instance) {
+  parent.appendChild(child)
+}
+
+export function createTextInstance(content: string) {
+  return document.createTextNode(content)
+}
+
+export const appendChildToContainer = appendInitialChild
