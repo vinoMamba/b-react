d8cbf3201493a100ac06472afa0e187782b18c7a
vino <vino0908@outlook.com>
2023-07-27 22:08:14 +0800

    调整flag

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -1,8 +1,8 @@
 export type Flags = typeof NoFlags | typeof Placement | typeof Update | typeof ChildDeletion
 
-export const NoFlags = 0b0000001
-export const Placement = 0b0000010
-export const Update = 0b0000100
-export const ChildDeletion = 0b0001000
+export const NoFlags = 0b0000000
+export const Placement = 0b0000001
+export const Update = 0b0000010
+export const ChildDeletion = 0b0000100
 
 export const MutationMarks = Placement | Update | ChildDeletion
